87e5a53fe946014ad1da59497a7e55db22f83838
Eij,Emiel E.M.H <i373939@fhict.nl>
2024-10-10 13:11:08 +0200

    added the second function using the fold function  that exist for arrays in scala

--- task2/src/main/scala/Main.scala
+++ task2/src/main/scala/Main.scala
@@ -4,8 +4,9 @@
   println("QuadraticEquation X1, X2, and RealSol, when A = 2, B = 1 and C = 2?")
   println(QuadraticEquation(2, 1, 2))	// should print X1 = -1.0, X2 = 1.0, and RealSol = false
 
-  
-  
+  var array = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  println("Sum of Array: " + sumOFArray(array))
+  println("Length of Array: " + lengthOFArray(array))
 
 // from ass 3 task 1
 // proc {QuadraticEquation A B C ?RealSol ?X1 ?X2}. X1 and X2 should bind to the real
@@ -23,4 +24,15 @@ def QuadraticEquation(A: Double, B: Double, C: Double): (Boolean, Option[Double]
   } else {
     (false, None, None)  // No real solutions
   }
+}
+
+// From ass 3 task 
+// Write a function fun {RightFold List Op U}, which goes through a list recursively and, through the use
+// of a combining operation Op, accumulates and returns a result. U is the neutral elements for the operation.
+def sumOFArray(arr: Array[Int]): Int = {
+  arr.foldRight(0)((x, y) => x + y)
+}
+
+def lengthOFArray(arr: Array[Int]): Int = {
+  arr.foldRight(0)((x, y) => 1 + y)
 }
